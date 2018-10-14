7430c0e4562f1618b9559b7dcff98a4ebdd25b83
Ricky Kuang <rkuang26@gmail.com>
2018-10-14 12:54:36 -0700

    wired event logic

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -9,51 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var billAmountField: UITextField!
-    @IBOutlet weak var tipSelector: UISegmentedControl!
-    @IBOutlet weak var tipAmountField: UITextField!
-    @IBOutlet weak var totalAmountField: UITextField!
     
-    @IBAction func calculateTip(_ sender: Any) {
-        if let billAmount = Double(billAmountField.text!) {
-            var tipPercentage: Double = 0
-            
-            switch tipSelector.selectedSegmentIndex {
-            case 0:
-                tipPercentage = 0.15
-            case 1:
-                tipPercentage = 0.18
-            case 2:
-                tipPercentage = 0.20
-            default:
-                break
-            }
-            
-            let roundedBillAmount = round(100 * billAmount) / 100
-            let tipAmount = roundedBillAmount * tipPercentage
-            let roundedTipAmount = round(100*tipAmount)/100
-            let totalAmount = roundedBillAmount + roundedTipAmount
-            
-            if (!billAmountField.isEditing) {
-                billAmountField.text = String(format: "%.2f", roundedBillAmount)
-            }
-            tipAmountField.text = String(format: "%.2f", roundedTipAmount)
-            totalAmountField.text = String(format: "%.2f", totalAmount)
-        } else {
-            billAmountField.text = ""
-            tipAmountField.text = ""
-            totalAmountField.text = ""
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var splitAmountLabel: UILabel!
+
+    var roundedBillAmount: Float = 0.00
+    var roundedTipAmount: Float = 0.00
+    var roundedTotalAmount: Float = 0.00
+    var roundedTipPercentage: Float = 0.15
+    var roundedSplitAmount: Float = 0.00
+    var persons: Float = 1.0
+    
+    @IBAction func updateTipPercentage(_ sender: UISlider) {
+        roundedTipPercentage = round(100 * sender.value) / 100
+        tipPercentageLabel.text = String(format: "%.0f%%", roundedTipPercentage * 100)
+        
+        let tipAmount = roundedBillAmount * roundedTipPercentage
+        roundedTipAmount = round(100 * tipAmount) / 100
+        roundedTotalAmount = roundedBillAmount + roundedTipAmount
+        
+        let splitAmount = roundedTotalAmount / persons
+        roundedSplitAmount = round(100 * splitAmount) / 100
+        updateLabels()
+    }
+    
+    @IBAction func updateSplitLabel(_ sender: UISlider) {
+        persons = sender.value
+        persons.round(.toNearestOrAwayFromZero)
+        sender.value = persons
+        splitLabel.text = String(format: "%.0f", persons)
+        
+        let str = totalAmountLabel.text!
+        let totalAmount = Float(str[str.index(after: str.startIndex)...])!
+        
+        let splitAmount = totalAmount / persons
+        roundedSplitAmount = round(100 * splitAmount) / 100
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        tipAmountLabel.text = String(format: "$%.2f", roundedTipAmount)
+        totalAmountLabel.text = String(format: "$%.2f", roundedTotalAmount)
+        splitAmountLabel.text = String(format: "$%.2f", roundedSplitAmount)
+    }
+    
+    func calculateTip() {
+        let tipAmount = roundedBillAmount * roundedTipPercentage
+        roundedTipAmount = round(100 * tipAmount) / 100
+        roundedTotalAmount = roundedBillAmount + roundedTipAmount
+        let splitAmount = roundedTotalAmount / persons
+        roundedSplitAmount = round(100 * splitAmount) / 100
+        updateLabels()
+    }
+    
+    @IBAction func billAmountChanged(_ sender: UITextField) {
+        if let amountString = sender.text?.currencyInputFormatting() {
+            sender.text = amountString
+            let billAmount = amountString.currencyToFloat()!
+            roundedBillAmount = round(100 * billAmount) / 100
+            calculateTip()
         }
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
     }
     
     override func didReceiveMemoryWarning() {
@@ -61,6 +86,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension String {
+    
+    // formatting text for currency textField
+    func currencyInputFormatting() -> String {
+        
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyAccounting
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        
+        var amountWithPrefix = self
+        
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
+        
+        let double = (amountWithPrefix as NSString).doubleValue
+        number = NSNumber(value: (double / 100))
+        
+        // if first number is 0 or all numbers were deleted
+        guard number != 0 as NSNumber else {
+            return "$0.00"
+        }
+        
+        return formatter.string(from: number)!
+    }
+    
+    func currencyToFloat() -> Float? {
+        return Float(self[self.index(after:self.startIndex)...])
+    }
 }
 
