6e5f71cb3e5ff43b1be66a5a5a5d6c521a2c721c
Ricky Kuang <rkuang26@gmail.com>
2018-10-16 22:37:28 -0700

    adjusted views

--- TipPro/DesignableView.swift
+++ TipPro/DesignableView.swift
@@ -21,33 +21,6 @@ class DesignableView: UIView {
     }
     
     @IBInspectable
-    var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-    
-    @IBInspectable
-    var borderColor: UIColor? {
-        get {
-            if let color = layer.borderColor {
-                return UIColor(cgColor: color)
-            }
-            return nil
-        }
-        set {
-            if let color = newValue {
-                layer.borderColor = color.cgColor
-            } else {
-                layer.borderColor = nil
-            }
-        }
-    }
-    
-    @IBInspectable
     var shadowRadius: CGFloat {
         get {
             return layer.shadowRadius
