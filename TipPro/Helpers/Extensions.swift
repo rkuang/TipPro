fd0fb2e23279096b3df60a2e27b27ab69dc197b2
Ricky Kuang <rkuang26@gmail.com>
2018-11-08 15:38:39 -0800

    converting string to int and int keyboard

--- TipPro/Helpers/Extensions.swift
+++ TipPro/Helpers/Extensions.swift
@@ -38,24 +38,6 @@ extension String {
         return formatter.string(from: number)!
     }
     
-    func percentageInputFormatting() -> String {
-        var number: NSNumber!
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .percent
-        formatter.maximumFractionDigits = 2
-        formatter.minimumFractionDigits = 2
-        
-        var amountWithSuffix = self
-        
-        let regex = try! NSRegularExpression(pattern: "[^.,0-9]", options: .caseInsensitive)
-        amountWithSuffix = regex.stringByReplacingMatches(in: amountWithSuffix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
-        
-        let double = (amountWithSuffix as NSString).doubleValue
-        number = NSNumber(value: double)
-        
-        return formatter.string(from: number)!
-    }
-    
     func currencyToDouble() -> Double {
         var amountWithPrefix = self
         let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
@@ -63,11 +45,8 @@ extension String {
         return Double(amountWithPrefix)!/100
     }
     
-    func percentageToFloat() -> Float {
-        var amountWithSuffix = self
-        let regex = try! NSRegularExpression(pattern: "[^.,0-9]", options: .caseInsensitive)
-        amountWithSuffix = regex.stringByReplacingMatches(in: amountWithSuffix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
-        return Float(amountWithSuffix)!/100
+    func toInt() -> Int {
+        return Int(self) ?? 0
     }
 }
 
